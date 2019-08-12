592fab5865b6ea076f20ad52d0db4dd62b5e9952
briantfan <briantfan@gmail.com>
2019-08-11 19:13:21 -0700

    Bike

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from pybricks import ev3brick as brick
 from pybricks.parameters import Button
 from pybricks.tools import print, wait, StopWatch
 import robot
+import bike
+
+bike.watchDistance()
 
 robot = robot.Robot()
 stopwatch = StopWatch()
@@ -17,7 +20,6 @@ while True:
 #    if stopwatch.time() >= 10000 and stopwatch.time() < 10100:
 #        print("10 sec: ", count)
 #    count = count + 1
-
     if Button.UP in brick.buttons():
         start_angle = robot.gyro_sensor.angle()
         robot.backward(30)
@@ -37,25 +39,3 @@ while True:
         robot.forward(30)
         brick.display.text(robot.gyro_sensor.angle() - start_angle)
 
-"""
-    if Button.UP in brick.buttons():
-        start_angle = robot.gyro_sensor.angle()
-        robot.move_forward(50)
-        brick.display.text(robot.gyro_sensor.angle() - start_angle)
-    elif Button.LEFT in brick.buttons():
-        start_angle = robot.gyro_sensor.angle()
-        robot.accelerate_forward(30) 
-        brick.display.text(robot.gyro_sensor.angle() - start_angle)
-    elif Button.CENTER in brick.buttons():
-        start_angle = robot.gyro_sensor.angle()
-        robot.accelerate_forward_to_max_speed(40) 
-        brick.display.text(robot.gyro_sensor.angle() - start_angle)
-    elif Button.RIGHT in brick.buttons():
-        start_angle = robot.gyro_sensor.angle()
-        robot.smooth_move_forward(30) 
-        brick.display.text(robot.gyro_sensor.angle() - start_angle)
-    elif Button.DOWN in brick.buttons():
-        start_angle = robot.gyro_sensor.angle()
-        robot.forward(30)
-        brick.display.text(robot.gyro_sensor.angle() - start_angle)
-"""
