77bf37098738a682c5a899ed512ab95240649a62
greglockwood555 <greg.lockwood@gmail.com>
2019-09-16 20:25:51 -0700

    crane and black block changes

--- Crane_Drop.py
+++ Crane_Drop.py
@@ -7,10 +7,14 @@ from pybricks.parameters import (Port, Stop, Direction, Button, Color,
                                  SoundFile, ImageFile, Align)
 from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
+import time
 import robot
 
 def run(robot):
-    robot.backward(32.5)
+    robot.backward(31.8)
+    time.sleep(1)
     robot.forward(12.1)
-    robot.turn_right(90)
-    robot.forward(20)
+    #robot.turn_right(90)
+    robot.right_wheel.run_angle(200,110,Stop.BRAKE,False)
+    robot.left_wheel.run_angle(-200,110,Stop.BRAKE)
+    robot.forward(19.5)
