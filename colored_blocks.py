4df8b733c7d3bf05df05fd0fc44e76c5f88fd21c
briantfan <briantfan@gmail.com>
2019-10-12 15:58:54 -0700

    colored block todos

--- colored_blocks.py
+++ colored_blocks.py
@@ -17,20 +17,20 @@ def run(robot):
    # lifting the thing keeping the red blocks with the robot
    robot.left_motor.run_angle (300, -540)
    # getting to the tan circle
-   robot.line_follow_to_divot() 
-   robot.turn_left(85, max_speed=-100)
-   robot.forward (16)
+   robot.line_follow_to_divot()  # TODO: check to see if we should be turning slower here
+   robot.turn_left(85, max_speed=-100) # TODO: is absolute turn more accurate here?
+   robot.forward(16)
    # backing out of the tan circle
-   robot.backward (9.5)
+   robot.backward(9.5)
    # getting to the elevator
-   robot.turn_right (25)
-   robot.forward (3)
-   robot.line_follow (10)
-   robot.forward (4)
+   robot.turn_right(25)
+   robot.forward(3) # TODO: move forward to line, then turn
+   robot.line_follow(10) # TODO: line follow to end
+   robot.forward(4)
    # getting the elevator to balance
    wait(2000)
    # going up the ramp
-   robot.backward (15.5)
+   robot.backward(15.5) # TODO back up to line
    robot.turn_left(85)
-   robot.line_follow(10)
-   robot.forward (25)
+   robot.line_follow(10) # TODO line follow to black line
+   robot.forward(25)
