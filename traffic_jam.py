6a51e0e1206a7bff9d9bb8a23cd63e79b3962900
briantfan <briantfan@gmail.com>
2019-11-23 19:37:34 -0800

    fix drone, clip gyro correction

--- traffic_jam.py
+++ traffic_jam.py
@@ -13,22 +13,36 @@ def run(robot):
     # Move to the traffic jam.
     robot.backward(19.25)
     # Pull up the traffic jam.
-    robot.left_motor.run_angle(800, 600)
-    robot.left_motor.run_angle(-800, 170)
+    robot.left_motor.run_angle(800, 650)
+    robot.left_motor.run_angle(-800, 270)
     # Turn and move toward the line.
     robot.turn_left(25)
     robot.move_to_line_backwards()
     robot.turn_right(220)
     # Keep line following to go up the side of the board.
     robot.line_follow(25, speed=-120)
-    # Knock the bar down on the swing.
+    # Knock the bar down on the swing and turn towards the bridge.
     robot.turn_left_pivot_back(105, min_speed=-100)
+    # Move up to the line.
     robot.forward(8)
     robot.move_to_line(5)
     robot.turn_right(65)
+    # Follow the line to flip the elevator.
     robot.line_follow(12)
     robot.line_follow_to_end(2)
-    robot.backward(6, max_speed=80)
-    robot.backward(18, decel=1000)
-    robot.turn_right(24, min_speed=120)
-    robot.backward(58, min_speed=200, max_speed=600)
+    # Goes to do build mission
+    robot.backward(5, max_speed=80)
+    robot.backward (6)
+    robot.turn_left_pivot_back (146)
+    robot.left_motor.run_angle (-800, 320)
+    robot.backward (9)
+    robot.left_motor.run_angle (-800, 50)
+    robot.turn_right (90)
+    robot.forward (1)
+    robot.turn_left (45)
+    robot.forward (4)
+    robot.turn_left (45)
+    robot.forward (10)
+    #robot.backward(18, decel=1000)
+    #robot.turn_right(21, min_speed=120)
+    #robot.backward(58, min_speed=200, max_speed=800)
