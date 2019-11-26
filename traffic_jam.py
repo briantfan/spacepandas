9295509855c7981094946c7758225f982a55a5d2
greglockwood555 <greg.lockwood@gmail.com>
2019-11-25 21:43:25 -0800

    Nov 25th changes

--- traffic_jam.py
+++ traffic_jam.py
@@ -20,7 +20,7 @@ def run(robot):
     robot.move_to_line_backwards()
     robot.turn_right(220)
     # Keep line following to go up the side of the board.
-    robot.line_follow(25, speed=-120)
+    robot.line_folow(23, speed=-120)
     # Knock the bar down on the swing and turn towards the bridge.
     robot.turn_left_pivot_back(105, min_speed=-100)
     # Move up to the line.
@@ -28,25 +28,29 @@ def run(robot):
     robot.move_to_line(5)
     robot.turn_right(65)
     # Follow the line to flip the elevator.
-    robot.line_follow(12)
-    robot.line_follow_to_end(2)
-    # Goes to do build mission --------------------------
-    #robot.backward(5, max_speed=80)
-    #robot.backward (6)
-    # Sets up to knock down pegs ------------------------
-    #robot.turn_left_pivot_back (140)
-    #robot.left_motor.run_angle (-800, 315)
-    #robot.backward (7)
-    # Does Build Mission --------------------------------
-    #robot.left_motor.run_angle (-800, 50)
-   # robot.turn_right (90)
-    # Exits ----------------------------------------------
-    #wait (3000) # Wait 3 seconds
-    #robot.forward (5)
-    #robot.turn_right (55)
-   # robot.forward (20, min_speed=200)
-    # Goes back to base ----------------------------------
-    #robot.forward (20, min_speed=200)
-    robot.backward(18, decel=1000)
-    robot.turn_right(21, min_speed=120)
-    robot.backward(58, min_speed=200, max_speed=800)
+    robot.line_follow(14)
+    robot.line_follow_to_end(3)
+    wait (1000) # Wait 1 second
+    # Goes to do build mission
+    robot.backward(5, max_speed=80, gyro_correct=0)
+    robot.backward (6, gyro_correct=0)
+    # Sets up to knock down two blue pegs
+    robot.turn_left_pivot_back (143)
+    robot.left_motor.run_angle (-800, 315)
+    robot.backward (8)
+    # Does Build Mission
+    robot.left_motor.run_angle (-800, 45)
+    robot.turn_right (90)
+    # Pause to think, DELETE LATER AFTER DONE!!!!!!!
+    wait (1000) # Wait 1 second
+    robot.forward (6) #backs out from building
+    robot.turn_right (55)
+    wait (2000) #     DELETE LATER AFTER DONE!!!!!!!
+    robot.backward (10)
+    robot.turn_right (10)
+    robot.backward (100)
+    # Goes back to base
+    # robot.forward (20, min_speed=200)
+    #robot.backward(18, decel=1000)
+    #robot.turn_right(21, min_speed=120)
+    #robot.backward(58, min_speed=200, max_speed=800)
